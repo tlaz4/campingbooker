5fb96c2855bace97e56069a80a9c9016328a61f7
Tyler <tlazaren@ualberta.ca>
2020-08-05 15:22:17 -0600

    added test for user api

--- services/web/project/api/users.py
+++ services/web/project/api/users.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restx import Resource, Api
 from marshmallow import ValidationError
+from sqlalchemy.exc import DataError
 
 from project.api.models import User
 from project.api.validators import UserValidator
@@ -20,7 +21,7 @@ class Ping(Resource):
 
 class UsersList(Resource):
 
-	def __init__(self):
+	def __init__(self, name):
 		self.validator = UserValidator()
 		super(UsersList, self).__init__()
 
@@ -39,47 +40,67 @@ class UsersList(Resource):
 
 	# post a user
 	def post(self):
+
+		response_object = {
+			'status': 'fail',
+			'message': 'Invalid payload',
+
+		}
+
 		post_data = request.get_json()
+
+		if not post_data:
+			return response_object, 401
 
 		try:
 			validated_data = self.validator.load(post_data)
-		except ValidationError as err:
-			response_object = {
-				'status': 'fail',
-				'message': 'The posted data is not valid',
-				'errors': err.messages
+			user = User.query.filter_by(email=validated_data['email']).first()
 
-			}
+			if not user:
+				user = User(**validated_data)
+				db.session.add(user)
+				db.session.commit()
 
+				response_object['status'] = 'success'
+				response_object['message'] = f'{user.email} was added!'
+
+				return response_object, 201
+
+			else:
+				response_object['message'] = 'Sorry that user already exists' 
+				return response_object, 401
+
+		except ValidationError:
 			return response_object, 401
-
-		email = post_data.get('email')
-
-		db.session.add(User(email=email))
-		db.session.commit()
-
-		response_object = {
-			'status': 'success',
-			'message': f'{email} was added!'
-		}
-
-		return response_object, 201
 
 
 class Users(Resource):
 	# get a user by id
 	def get(self, user_id):
-		user = User.query.filter_by(id=user_id).first()
 		response_object = {
-			'status': 'success',
-			'data' : user.to_json()
+			'status': 'fail',
+			'message': 'User does not exist',
 		}
 
-		return response_object, 200
+		try:
+			user = User.query.filter_by(id=user_id).first()
 
+			if not user:
+				return response_object, 404
+
+			else:
+				response_object = {
+					'status': 'success',
+					'data' : user.to_json()
+				}
+
+				return response_object, 200
+
+		except (ValueError, DataError):
+			return response_object, 404
 
 
 api.add_resource(Ping, '/api/users/ping')
 api.add_resource(UsersList, '/api/users')
-api.add_resource(Users, '/api/users/<string:user_id>')
+api.add_resource(Users, '/api/users/<user_id>')
 
--- services/web/project/tests/test_users.py
+++ services/web/project/tests/test_users.py
@@ -25,6 +25,102 @@ class TestUserService(BaseTestCase):
             self.assertIn('tlazaren@ualberta.ca', data['data']['email'])
             self.assertIn('success', data['status'])
 
+    # test user doesnt exist
+    def test_get_user_doesnt_exist(self):
+        with self.client:
+            response = self.client.get('/api/users/1')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    # test bad id provided
+    def test_get_user_bad_id(self):
+        with self.client:
+            response = self.client.get('/api/users/bad')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    # test adding a user
+    def test_add_user(self):
+        user = {'email': 'tlazaren@ualberta.ca'}
+
+        with self.client:
+            response = self.client.post('api/users', 
+                data=json.dumps(user),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('success', data['status'])
+            self.assertIn('tlazaren@ualberta.ca was added', data['message'])
+
+    # add a user with no email
+    def test_add_user_no_email(self):
+        user = {'phone': '7805546061'}
+
+        with self.client:
+            response = self.client.post('api/users',
+                data=json.dumps(user),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertIn('fail', data['status'])
+            self.assertIn('Invalid payload', data['message'])
+
+    # add a user with an incorrectly formatted email
+    def test_add_user_bad_email(self):
+        user = {'email': 'tlazarenualberta.ca'}
+
+        with self.client:
+            response = self.client.post('api/users',
+                data=json.dumps(user),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertIn('fail', data['status'])
+            self.assertIn('Invalid payload', data['message'])
+
+    # test adding a user where the email already exists
+    def test_add_user_already_exists(self):
+        user = create_user('tlazaren@ualberta.ca')
+
+        with self.client:
+            response = self.client.post('api/users',
+                data=json.dumps({
+                    'email': 'tlazaren@ualberta.ca'
+                }),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 401)
+            self.assertIn('fail', data['status'])
+            self.assertIn('Sorry that user already exists', data['message'])
+
+    # test getting all users
+    def test_get_all_users(self):
+        user1 = create_user('tlazaren@ualberta.ca')
+        user2 = create_user('testemail@test.com')
+
+        with self.client:
+            response = self.client.get('api/users')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertIn('success', data['status'])
+            self.assertIn('tlazaren@ualberta.ca', data['data']['users'][0]['email'])
+            self.assertIn('testemail@test.com', data['data']['users'][1]['email'])
+
+
+
 
 
 if __name__ == '__main__':
